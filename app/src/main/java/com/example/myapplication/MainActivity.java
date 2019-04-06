d0e0d83dae77f6097f6ef7cfaaead23170ab1fa9
oleksandr.voynarovskyy@ria.com <oleksandr.voynarovskyy@ria.com>
2019-04-07 00:27:11 +0300

    initial commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,56 +1,29 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.io.IOException;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     private DbWorker dbWorker;
-    private TextView defaultCity_1, defaultCity_2;
+    private TextView defaultCity, defaultCityTemp, defaultCity2, defaultCityTemp2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        defaultCity_1 = (TextView) findViewById(R.id.defaultCity_1);
-        defaultCity_2 = (TextView) findViewById(R.id.defaultCity_2);
-        get();
+        defaultCity = (TextView) findViewById(R.id.defaultCity);
+        defaultCityTemp = (TextView) findViewById(R.id.defaultCityTemp);
+        defaultCity2 = (TextView) findViewById(R.id.defaultCity2);
+        defaultCityTemp2 = (TextView) findViewById(R.id.defaultCityTemp2);
+        new OpenWeather("Vinnytsia");//.weatherData.body().getName());
+//        getWeatherByCityName("Kyiv");
+
     }
 
-    @SuppressWarnings("unchecked")
-    private void get() {
-            OpenWeatherHelper.getInstance()
-                    .getJSONApi()
-                    .getCurrentWeather("Vinnytsia")
-                    .enqueue(new Callback<CityResponseModel>() {
-                        @Override
-                        public void onResponse(@NonNull Call<CityResponseModel> call, @NonNull Response<CityResponseModel> response) {
-                            CityResponseModel post = response.body();
-                            Log.d("!!!", String.valueOf(post.getId()));
 
-//                        defaultCity_1.append(post.getName() + "\n");
-                        }
 
-                        @Override
-                        public void onFailure(@NonNull Call<CityResponseModel> call, @NonNull Throwable t) {
 
-                            toastMessage("Error occurred while getting request!");
-                        }
-                    });
-    }
-
-    private void toastMessage(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-    }
 }
--- app/src/main/java/com/example/myapplication/OpenWeatherAPI.java
+++ app/src/main/java/com/example/myapplication/OpenWeatherAPI.java
@@ -3,9 +3,12 @@ package com.example.myapplication;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface OpenWeatherAPI {
 
-    @GET("q={cityName}&units=metric&appid=8b3e8129ac9e59e711839ca1e82aae07")
-    Call<CityResponseModel> getCurrentWeather(@Path("cityName") String cityName);
+    final String APP_ID = "8b3e8129ac9e59e711839ca1e82aae07";
+
+    @GET("weather?q=vinnytsia&units=metric&appid=8b3e8129ac9e59e711839ca1e82aae07")
+    Call<CityResponseModel> getCurrentWeather();
 }
